5e8846d74e470d0821ed800f4ff33bee546e48fb
vinaykavuri <kavurivinay@gmail.com>
2020-03-06 14:09:53 -0500

    Initial commit

---
+++ app/src/main/java/com/kavurivinay/quizapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kavurivinay.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Gson gson = new Gson();
+    }
+}
