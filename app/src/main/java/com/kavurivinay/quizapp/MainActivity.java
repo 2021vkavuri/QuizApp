56621a1973ff7bd6a03985dd4d75288844ed5aca
vinaykavuri <kavurivinay@gmail.com>
2020-03-13 11:46:20 -0400

    lab06 C
    I added an object class that stores the information for the trivia questions, and made it so that it randomly picks one of the questions to display on the screen whenever the game starts or a correct answer is pressed

--- app/src/main/java/com/kavurivinay/quizapp/MainActivity.java
+++ app/src/main/java/com/kavurivinay/quizapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
@@ -15,19 +16,33 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
     Button startButton, correctButton, wrongButton1, wrongButton2, wrongButton3;
-    TextView myText, score;
+    TextView myText, score, question;
     int counter;
     boolean doLoop = true;
     final Handler h = new Handler();
     Gson gson;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    ArrayList<Trivia> triviaQuestions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        triviaQuestions = new ArrayList<Trivia>();
+        triviaQuestions.add(new Trivia("Established in 1607, what settlement was founded by John Smith in Virginia?", "Jamestown", "Roanoke", "Plymouth", "New York"));
+        triviaQuestions.add(new Trivia("Who is the only US President to serve two non-consecutive terms?", "Grover Cleveland", "Franklin D Roosevelt", "Theodore Roosevelt", "William Taft"));
+        triviaQuestions.add(new Trivia("What wonder of the ancient world was destroyed by arson the same night Alexander the Great was born?", "Temple of Artemis", "Lighthouse of Alexandria", "Colossus of Rhodes", "Hanging Gardens of Babylon"));
+        triviaQuestions.add(new Trivia("Who was the second man to set foot on the moon?", "Edwin Aldrin", "Neil Armstrong", "Michael Collins", "Charles Conrad"));
+
         sharedPreferences= getSharedPreferences("clickValues", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
         Gson gson = new Gson(); //converts class to a string to store in shared preferences
@@ -92,18 +107,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void correctAnswer(View view){
         counter += 1;
+        Trivia randQuestion = triviaQuestions.get((int)(triviaQuestions.size() * Math.random()));
+        question.setText(randQuestion.myQuestion);
+        correctButton.setText(randQuestion.myCorrect);
+        wrongButton1.setText(randQuestion.myWrong1);
+        wrongButton2.setText(randQuestion.myWrong2);
+        wrongButton3.setText(randQuestion.myWrong3);
     }
 
     public void startGame(View view) {
         setContentView(R.layout.game_scren);
+        question = findViewById(R.id.question);
         correctButton = findViewById(R.id.correct_answer);
         wrongButton1 = findViewById(R.id.wrong1);
         wrongButton2 = findViewById(R.id.wrong2);
         wrongButton3 = findViewById(R.id.wrong3);
+
+        Trivia randQuestion = triviaQuestions.get((int)(triviaQuestions.size() * Math.random()));
+        question.setText(randQuestion.myQuestion);
+        correctButton.setText(randQuestion.myCorrect);
+        wrongButton1.setText(randQuestion.myWrong1);
+        wrongButton2.setText(randQuestion.myWrong2);
+        wrongButton3.setText(randQuestion.myWrong3);
+
         doLoop = true;
         doAction(view);
     }
     public class Score {
         public int myScore = 0;
+    }
+    public class Trivia {
+        public String myQuestion, myCorrect, myWrong1, myWrong2, myWrong3;
+        public Trivia(String q, String c, String w1, String w2, String w3){
+            myCorrect = c;
+            myQuestion = q;
+            myWrong1 = w1;
+            myWrong2 = w2;
+            myWrong3 = w3;
+        }
     }
 }
